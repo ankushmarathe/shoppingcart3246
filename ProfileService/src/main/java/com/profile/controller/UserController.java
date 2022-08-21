7733253315dcb7f3e874e4c0abdf17afc83addbf
aditingale-hash <aditiingale2000@gmail.com>
2022-08-21 17:24:55 +0530

    void

---
+++ ProfileService/src/main/java/com/profile/controller/UserController.java
@@ -0,0 +1,66 @@
+package com.profile.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.profile.model.User;
+import com.profile.repository.UserRepository;
+
+@RestController
+public class UserController {
+
+	
+	@Autowired
+	private UserRepository  userRepository;
+	
+	@PostMapping("/user")
+	public User adduser( @RequestBody User user) {
+		
+		
+		return userRepository.save(user);
+	}
+	
+	@GetMapping("/user")
+	public List<User> getAllStaff(){
+		
+		return userRepository.findAll();
+	}
+	
+	@DeleteMapping("/user/{uid}")		
+	public void deleteStaff(@PathVariable("uid") Long uid) {
+		userRepository.deleteById(uid);
+		}
+	
+	@PutMapping("/user/update/{uid}")
+	public User updateUser(@PathVariable("uid") Long uid, @RequestBody User Newuser) {
+		
+		User userDB= userRepository.getById(uid);
+		
+		if(Newuser.getName() !=null) 
+			userDB.setName(Newuser.getName());
+			
+		if(Newuser.getAddress() !=null) 
+			userDB.setAddress(Newuser.getAddress());
+		if(Newuser.getRole() !=null) 
+			userDB.setRole(Newuser.getRole());
+					
+		if(Newuser.getMobile() !=null) 
+			userDB.setMobile(Newuser.getMobile());
+						
+		if(Newuser.getAge() !=0) 
+			userDB.setAge(Newuser.getAge());
+			
+		if(Newuser.getEmail() !=null) 
+			userDB.setEmail(Newuser.getEmail());
+		
+		return userRepository.save(userDB);
+}
+}
---
+++ ProfileService/src/main/java/com/profile/model/User.java
@@ -0,0 +1,87 @@
+package com.profile.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+     private Long userId;
+	@Column(nullable = false)
+	private String Name;
+	@Column(nullable = false)
+	private String Address;
+	@Column(nullable = false)
+	private String role;
+	
+	private Long mobile;
+	
+	private int age;
+	
+	@Column(nullable = false)
+	private String email;
+
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+	public String getName() {
+		return Name;
+	}
+
+	public void setName(String name) {
+		Name = name;
+	}
+
+	public String getAddress() {
+		return Address;
+	}
+
+	public void setAddress(String address) {
+		Address = address;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public Long getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(Long mobile) {
+		this.mobile = mobile;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+
+}

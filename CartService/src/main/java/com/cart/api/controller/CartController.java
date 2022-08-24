530bf2267fd9e22e0c0265e4f6674722befded86
ankushmarathe <83711609+ankushmarathe@users.noreply.github.com>
2022-08-24 13:57:01 +0530

    Gateway Issue Fixed

--- CartService/src/main/java/com/cart/api/controller/CartController.java
+++ CartService/src/main/java/com/cart/api/controller/CartController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cart.api.model.Cart;
@@ -18,25 +19,26 @@ import com.cart.api.repository.CartRepository;
 
 
 @RestController
+@RequestMapping("/cart")
 public class CartController {
 	@Autowired
 	private CartRepository cartRepository;
 	
 	// display the records from  cart
-	@GetMapping("/cart")
+	@GetMapping("/allcart")
 	public List<Cart> getCart() {
 		
 		return cartRepository.findAll();
 	}
 	//posting the records in the cart
-	@PostMapping("/cart")
+	@PostMapping("/addcart")
 	public Cart postCart(@RequestBody Cart cart)
 	{
 		return cartRepository.save(cart);
 		
 	}
 	//update cart
-	@PutMapping("/cart/{cId}")
+	@PutMapping("/editcart/{cId}")
 	public Cart updateCart(@PathVariable("cId") Long cId, @RequestBody Cart cart) {
 		Cart cart1=cartRepository.getById(cId);
 		
@@ -49,7 +51,7 @@ public class CartController {
 		return cartRepository.save(cart1);
 	}
 	//delete product in cart using cart id
-	@DeleteMapping("/cart/{cId}")
+	@DeleteMapping("/deletecart/{cId}")
 	public List<Cart> deleteCart(@PathVariable("cId") Long cId){
 		cartRepository.deleteById(cId);
 		return cartRepository.findAll();
